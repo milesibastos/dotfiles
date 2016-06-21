4b5eb345e63afaf389202d3daaf0bca1c0c516b8
Antônio Milesi Bastos <antonio@milesibastos.com>
2016-06-21 16:47:22 -0300

    install typeface designed for source code

--- install/brew.sh
+++ install/brew.sh
@@ -33,4 +33,7 @@ brew install diff-so-fancy
 # install neovim
 brew install neovim/neovim/neovim
 
-# exit 0
+# install typeface designed for source code
+brew cask install caskroom/fonts/font-hack
+brew cask install caskroom/fonts/font-droid-sans-mono-for-powerline
+# cd ~/Library/Fonts && curl -fLo "Droid Sans Mono for Powerline Nerd Font Complete.otf" https://raw.githubusercontent.com/ryanoasis/nerd-fonts/master/patched-fonts/DroidSansMono/complete/Droid%20Sans%20Mono%20for%20Powerline%20Nerd%20Font%20Complete.otf
