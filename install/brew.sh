8175a1743569a17299abc1115bd0ea08afa3b694
Antonio Milesi Bastos <antonio@milesibastos.com>
2016-08-12 15:04:05 -0300

    Setup install

--- install/brew.sh
+++ install/brew.sh
@@ -36,4 +36,8 @@ brew install zsh-autosuggestions
 # install neovim
 brew install neovim/neovim/neovim
 
-exit 0
+# install typeface designed for source code
+brew cask install caskroom/fonts/font-hack
+brew cask install caskroom/fonts/font-droid-sans-mono-for-powerline
+# cd ~/Library/Fonts && curl -fLo "Droid Sans Mono for Powerline Nerd Font Complete.otf" https://raw.githubusercontent.com/ryanoasis/nerd-fonts/master/patched-fonts/DroidSansMono/complete/Droid%20Sans%20Mono%20for%20Powerline%20Nerd%20Font%20Complete.otf
+# exit 0
